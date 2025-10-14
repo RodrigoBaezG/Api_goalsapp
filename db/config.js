8f87586439fedd3701813981bb89e1b51c7dc831
Rodrigo Baez <rkbaezgarcia@gmail.com>
2025-10-14 20:59:39 +0100

    database_url

--- db/config.js
+++ db/config.js
@@ -1,14 +1,25 @@
 const initOptions = {};
 const pgp = require('pg-promise')(initOptions);
 
-cn = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: 'localhost',
-    port: 5432,
-    database: 'goalsapp',
-};
+const connectionString = process.env.DATABASE_URL;
 
-const db = pgp(cn);
+const config = connectionString 
+    ? {
+        connectionString: connectionString,
+        // CLAVE: Configuración SSL necesaria para conectar a Render PostgreSQL desde tu servicio Express.
+        ssl: {
+            rejectUnauthorized: false, 
+        },
+    } 
+    : {
+        // Fallback para desarrollo local (usando las variables separadas o localhost)
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: 'localhost', // O process.env.DB_HOST
+        port: 5432,
+        database: 'goalsapp',
+    };;
+
+const db = pgp(config);
 
 module.exports = db;

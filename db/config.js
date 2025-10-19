626931c6bb51d0e14d71adef3df513b8c949b82a
Rodrigo Baez <rkbaezgarcia@gmail.com>
2025-10-19 19:54:11 +0100

    local environment

--- db/config.js
+++ db/config.js
@@ -12,8 +12,13 @@ const config = connectionString
         },
     } 
     : {
-
-        database: 'goalsapp',
+        // 💡 Bloque de configuración local: AÑADIR credenciales.
+        host: 'localhost',      // 99% de las veces es 'localhost'
+        port: 5432,             // 99% de las veces es 5432
+        database: 'goalsapp',   // El nombre de DB que creaste
+        // 👇👇 CLAVE: DEBES AÑADIR ESTO 👇👇
+        user: process.env.DB_USER,       // O el usuario que usaste para instalar Postgres (a menudo 'postgres')
+        password: process.env.DB_PASSWORD, // **Cambia esto** por la contraseña de tu usuario de Postgres.
     };
 
 const db = pgp(config);
